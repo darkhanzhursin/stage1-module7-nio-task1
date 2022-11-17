8fd5129cb5b11e5f1c2e721ef4bac0081f8e5153
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-11-17 19:19:58 +0600

    NIO task without Files

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -3,7 +3,8 @@ package com.epam.mjc.nio;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.file.Files;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 
 public class FileReader {
@@ -14,11 +15,22 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
 
-        try (RandomAccessFile aFile = new RandomAccessFile(file, "r")) {
-            String[] nameArr = Files.readAllLines(file.toPath()).get(0).split(":", 2);
-            String[] ageArr = Files.readAllLines(file.toPath()).get(1).split(":", 2);
-            String[] emailArr = Files.readAllLines(file.toPath()).get(2).split(":", 2);
-            String[] phoneArr = Files.readAllLines(file.toPath()).get(3).split(":", 2);
+        try (RandomAccessFile aFile = new RandomAccessFile(file, "r");
+            FileChannel inChannel = aFile.getChannel();) {
+            long fileSize = inChannel.size();
+
+            ByteBuffer buffer = ByteBuffer.allocate((int) fileSize);
+            inChannel.read(buffer);
+            buffer.flip();
+
+            // Now let's convert this ByteBuffer to String
+            String converted = new String(buffer.array(), "UTF-8");
+            String[] lines = converted.split("\n", 5);
+
+            String[] nameArr = lines[0].split(":",2);
+            String[] ageArr = lines[1].split(":", 2);
+            String[] emailArr = lines[2].split(":", 2);
+            String[] phoneArr = lines[3].split(":", 2);
 
             name = nameArr[1].trim();
             age = Integer.parseInt(ageArr[1].trim());
